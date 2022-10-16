bfaee57d1acc6e79fb4802a979db6d29f5cfa500
Nicole Hess <hess_nicole@bluewin.ch>
2022-10-16 23:13:45 +0200

    update cv & impressionen

---
+++ CV.js
@@ -0,0 +1,14 @@
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('animate__animated animate__fadeInRightBig');
+      }
+    });
+  });
+  
+  // Tell the observer which elements to track
+  observer.observe(document.querySelector('.timeline-entry'));
